117834cdeb3a164d2f48ecfc660cf32149ab402c
Sharath3002 <sharathmh2604@gmail.com>
2025-07-26 23:11:50 +0530

    Update

--- rough.py
+++
@@ -1,7 +0,0 @@
-
-def Palindrome(s):
-    new=''.join(char.lower() for char in s if char.isalnum())
-    if new==new[::-1]:
-        return True
-s=["A man, a plan, a canal: Panama"]
-print(Palindrome(s))
